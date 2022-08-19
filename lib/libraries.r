6823a27a2ce18db724bd928cb4456ef5ebd879aa
Regina <regina.blagotinsek@student.fmf.uni-lj.si>
2022-08-19 13:24:37 +0200

    nič

--- lib/libraries.r
+++ lib/libraries.r
@@ -12,3 +12,4 @@ options(gsubfn.engine="R")
 
 # Uvozimo funkcije za pobiranje in uvoz zemljevida.
 source("lib/uvozi.zemljevid.r", encoding="UTF-8")
+
--- lib/rstudio.r
+++ lib/rstudio.r
@@ -1,3 +1,4 @@
 if (Sys.getenv("HOME") == "/home/jovyan") {
   cat("[![Poženi v RStudiu](slike/RStudio.png)](../../rstudio)")
 }
+
